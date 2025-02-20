5613bfa5f5334dfdae961a5f13fcb49383119c2b
Naman Pandey <naman.0816@gmail.com>
2025-02-20 14:19:22 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
   },
-  typescript: {
-    // Disable type checking during build
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Disable ESLint during build
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   // Disable type checking during build
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   // Disable ESLint during build
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
