4f55958428270f962f71cd2811fb8bb4b4ca7c3f
Naman Pandey <naman.0816@gmail.com>
2025-02-27 18:25:05 +0530

    some more app fixes

--- app/(unprotected)/sign-in/page.tsx
+++ app/(unprotected)/sign-in/page.tsx
@@ -4,6 +4,9 @@ import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import db from "@/lib/db";
 
+// Disable caching to render dynamically
+export const revalidate = 0; // or cache: 'no-store'
+
 export default async function SignInPage() {
   const user = await getCurrentUser();
   if (user) {
--- app/(unprotected)/sign-up/page.tsx
+++ app/(unprotected)/sign-up/page.tsx
@@ -3,6 +3,9 @@ import { SignUpClient } from "../_components/sign-up-client";
 import { redirect } from "next/navigation";
 import { getAllRoomNo } from "@/actions/get-all-room-no";
 
+// Disable caching to render dynamically
+export const revalidate = 0; // or cache: 'no-store'
+
 export default async function SignUpPage() {
   const user = await getCurrentUser();
 
